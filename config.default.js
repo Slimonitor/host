4f0fdf39267c462293ae37b61f1f818cf687eb18
Robin Tail <robin_tail@me.com>
2018-12-27 14:14:28 +0100

    Minor indentation fix

--- config.default.js
+++ config.default.js
@@ -1,10 +1,10 @@
 module.exports = {
-        host: {
-            name: 'Host'
-        },
-        server: {
-            address: 'http://localhost:4080'
-        },
-        updateInterval: 1000,
-        sendInterval: 60000
+    host: {
+        name: 'Host'
+    },
+    server: {
+        address: 'http://localhost:4080'
+    },
+    updateInterval: 1000,
+    sendInterval: 60000
 };
